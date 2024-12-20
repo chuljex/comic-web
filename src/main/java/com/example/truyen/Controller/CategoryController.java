3aae84d887eaf1050b0f44347c39bb9da7b3f6a8
Sinh Nguyen <sinh.nt1@urbox.vn>
2024-12-20 09:39:06 +0700

    fix: category detail

--- src/main/java/com/example/truyen/Controller/CategoryController.java
+++ src/main/java/com/example/truyen/Controller/CategoryController.java
@@ -1,9 +1,11 @@
 package com.example.truyen.Controller;
 
 import com.example.truyen.Entity.Category;
+import com.example.truyen.Entity.Comic;
 import com.example.truyen.Entity.ComicStatus;
 import com.example.truyen.Entity.Country;
 import com.example.truyen.Service.CategoryService;
+import com.example.truyen.Service.ComicService;
 import com.example.truyen.Service.ComicStatusService;
 import com.example.truyen.Service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -28,15 +31,44 @@ public class CategoryController {
     @Autowired
     private CountryService countryService;
 
+    @Autowired
+    private ComicService comicService;
+
     @GetMapping("/{id}")
-    public String getCategoryById(@PathVariable Long id, Model model) {
+    public String getCategoryById(
+            @PathVariable Long id,
+            @RequestParam(required = false) Integer status,
+            @RequestParam(required = false) Integer country,
+            Model model
+    ) {
+        List<Category> categories = categoryService.getAllCategory();
         Category category = categoryService.getCategoryById(id);
         List<ComicStatus> comicStatuses = comicStatusService.getAllComicStatus();
         List<Country> countries = countryService.getAllCountry();
 
+        model.addAttribute("selectedCategoryId", id);
+        model.addAttribute("categories", categories);
         model.addAttribute("category", category);
         model.addAttribute("comicStatuses", comicStatuses);
         model.addAttribute("countries", countries);
+
+        filterComics(id, status, country, model);
         return "category/detail";
+    }
+
+    public void filterComics(
+            @PathVariable Long categoryId,
+            @RequestParam(required = false) Integer status,
+            @RequestParam(required = false) Integer country,
+            Model model) {
+
+        // Gọi service để lấy dữ liệu filter
+        List<Comic> comics = comicService.filterComics(categoryId, status, country);
+
+        // Đưa dữ liệu vào model để Thymeleaf xử lý
+        model.addAttribute("comics", comics);
+        model.addAttribute("categoryId", categoryId);
+        model.addAttribute("status", status);
+        model.addAttribute("country", country);
     }
 }
